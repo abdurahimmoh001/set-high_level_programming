1a741ad7b6f86c81f1fd7e28e079aaa3bc7277c7
Abdurahim Mohammed <abdurahimmohammed001@gmail.com>
2026-08-17 13:52:26 +0300

    TDD

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -1,76 +1,47 @@
 #!/usr/bin/python3
+"""100-matrix_mul module.
+
+Defines a function that multiplies two matrices.
 """
 
-This module contains a function that multiplies 2 matrices
 
-"""
+def validate_matrix(m, name):
+    """Validate that m is a proper rectangular matrix of numbers."""
+    if not isinstance(m, list):
+        raise TypeError("{} must be a list".format(name))
+
+    if not all(isinstance(row, list) for row in m):
+        raise TypeError("{} must be a list of lists".format(name))
+
+    if len(m) == 0 or all(len(row) == 0 for row in m):
+        raise ValueError("{} can't be empty".format(name))
+
+    if not all(isinstance(n, (int, float))
+               for row in m for n in row):
+        raise TypeError(
+            "{} should contain only integers or floats".format(name))
+
+    if len(set(len(row) for row in m)) > 1:
+        raise TypeError(
+            "each row of {} must be of the same size".format(name))
 
 
 def matrix_mul(m_a, m_b):
-    """This function multiplies two matrices
-
-    Args:
-        m_a (list of lists of int/float): Matrix to be multiplied
-        m_b (list of lists of int/float): Matrix to be multiplied
-
-    Raises:
-        TypeError: If m_a or m_b is not a list
-        TypeError: If m_a or m_b is not a list of lists
-        TypeError: If one element of list of lists is not int/float
-        TypeError: If row of m_a or m_b are not the same size
-        ValueError: If m_a or m_b is empty
-        ValueError: If m_a and m_b cannot be multiplied
-
-    Returns:
-        A new list which is the outcome of the multiplication
-
-    """
-
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    if not all((isinstance(element, int) or isinstance(element, float))
-               for element in [number for row in m_a for number in row]):
-        raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(element, int) or isinstance(element, float))
-               for element in [number for row in m_b for number in row]):
-        raise TypeError("m_b should contain only integers or floats")
-
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+    """Multiply two matrices and return the resulting matrix."""
+    validate_matrix(m_a, "m_a")
+    validate_matrix(m_b, "m_b")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    matrix1 = []
-    for i in range(len(m_b[0])):
-        my_row = []
-        for j in range(len(m_b)):
-            my_row.append(m_b[j][i])
-        matrix1.append(my_row)
+    result = []
+    for i in range(len(m_a)):
+        new_row = []
+        for j in range(len(m_b[0])):
+            total = 0
+            for k in range(len(m_b)):
+                total += m_a[i][k] * m_b[k][j]
+            new_row.append(total)
+        result.append(new_row)
 
-    matrix2 = []
-    for row in m_a:
-        my_row = []
-        for column in matrix1:
-            product = 0
-            for m in range(len(matrix1[0])):
-                product += row[m] * column[m]
-            my_row.append(product)
-        matrix2.append(my_row)
-
-    return matrix2
+    return result
