7995b9ca3aadf57bb61c66a78ee5be9bc8d3dd56
Abdurahim Mohammed <abdurahimmohammed001@gmail.com>
2026-08-17 13:38:35 +0300

    TDD

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python3
+"""
 
-""" add_integer
+This module has one function that adds up 2 integers
 
-Adds two integers (a, b) and returns integer sum
-Floats get converted to integers, all others raise TypeError
 """
 
 
 def add_integer(a, b=98):
-    """ add_integer - adds two integers (a, b)
-    Returns: integer sum
+    """Return the sum of two integers or floats as integers
+
+    Args:
+        a: first argument
+        b: second argument
+
+    Returns:
+        Sum of the two arguments
+
+    Raises:
+        TypeError: If either of the arguments not an integer or a float
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    return a + b
-
-
-if __name__ == '__main__':
-    print(add_integer(1, 2))
-    print(add_integer(100, -2))
-    print(add_integer(2))
-    print(add_integer(100.3, -2))
-    try:
-        print(add_integer(4, "School"))
-    except Exception as e:
-        print(e)
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
-
-    try:
-        print(add_integer(1.99, 3.5))
-    except Exception as e:
-        print(e)
-
-    try:
-        print(add_integer(None))
-    except Exception as e:
-        print(e)
+    return (int(a) + int(b))
