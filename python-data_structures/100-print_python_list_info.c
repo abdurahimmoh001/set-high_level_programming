e9d142f874fe3449d19f5be9a1fd4b9c86925ebb
abdurahimmoh001 <abdurahimmohammed001@gmail.com>
2026-08-20 00:59:16 -0700

    SET

---
+++ python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+    PyListObject *list = (PyListObject *)p;
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = list->allocated;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+
+    for (Py_ssize_t i = 0; i < size; i++)
+    {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *type_name = Py_TYPE(item)->tp_name;
+        printf("Element %ld: %s\n", i, type_name);
+    }
+}
